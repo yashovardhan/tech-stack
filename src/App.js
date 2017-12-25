ecdcd4d4191e10884086313f3e3bca6178f26db7
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-25 05:31:51 +0530

    render list of items using ListView

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
   return (
-      <Provider store={createStore(reducers)}>
-      <View />
-      </Provider>
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
   );
 };
 
