49dbf9d3a0f95d43250e68a920be6715b13a373d
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-25 06:30:30 +0530

    Add LayoutAnimation.spring()

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { library, expanded } = this.props;
 
